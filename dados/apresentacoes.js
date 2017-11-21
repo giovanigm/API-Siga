32a72bd7fdbece80f145bd1e0151be2434f58686
Giovani <ggmelone@gmail.com>
2017-11-20 22:18:38 -0200

    atualizando apresentacoes.js materiais.js

--- dados/apresentacoes.js
+++ dados/apresentacoes.js
@@ -11,42 +11,126 @@
 
 module.exports = {
     'HSO003': {
-        titulo: '',
-        apresentacao: '',
-        area: '',
-        ementa: '',
-        objetivo: '',
+        titulo: 'Ética e Responsabilidade Profissional',
+        apresentacao: 'Ética interfere com o desempenho organizacional, pois define conceitos de postura sócioresponsável.',
+        area: 'Humanas',
+        ementa: 'Liberdade e Responsabilidade\nética humana\nComportamento Cultural e social dos brasileiros\nética Profissional\nética empresarial\nCidadania Corporativa',
+        objetivo: 'Apresentar para o Aluno sobre o que é ética e como ela funciona e suas responsabilidades profissionais',
         cargas: {
-            semanais: 0,
-            teoricas: 0,
+            semanais: 2,
+            teoricas: 14,
             praticas: 0,
-            totais: 0,
+            totais: 14,
         }
     },
     'IIA002': {
-        titulo: '',
-        apresentacao: '',
-        area: '',
-        ementa: '',
-        objetivo: '',
+        titulo: 'Inteligência Artificial',
+        apresentacao: 'O que é IA\nO estudo da Inteligência Humana vêm sendo estudada a muito tempo à Fato;\nFilósofos têm tentado entender como ver, aprender, lembrar e raciocinar poderiam ou deveriam ser feitos;\nIA seria então uma forma de representar o raciocínio humano através de algoritmos, software e hardware, isto é através de Máquinas.',
+        area: 'Lógico',
+        ementa: 'Fundamentos e paradigmas da Inteligência Artificial (IA); \nTécnicas de IA aplicadas à solução de problemas;\nSistemas baseados em:\nConhecimento;Planejamento;Aprendizagem',
+        objetivo: 'Aplicar os conceitos de Inteligência Artificial;',
         cargas: {
-            semanais: 0,
-            teoricas: 0,
-            praticas: 0,
-            totais: 0,
+            semanais: 4,
+            teoricas: 14,
+            praticas: 14,
+            totais: 28,
         }
     },
     'ITI004': {
-        titulo: '',
-        apresentacao: '',
-        area: '',
-        ementa: '',
-        objetivo: '',
+        titulo: 'Gestão e Governança de Tecnologia da Informação',
+        apresentacao: 'O que é Gestão\nGestão significa gerenciamento, administração, onde existe uma instituição, uma empresa, uma entidade social de pessoas, a ser gerida ou administrada.',
+        area: 'Humanas',
+        ementa: 'Gestão: definições e conceitos\nMelhores Práticas de Gestão\nIntegração TI e Negócios\nTI e o Plano de Negócios\nGestão de TI\nGovernança de TI',
+        objetivo: 'Mostrar como é a Gestão e Governança de TI dentro de uma empresa e seus conceitos basicos',
         cargas: {
-            semanais: 0,
-            teoricas: 0,
+            semanais: 4,
+            teoricas: 28,
             praticas: 0,
-            totais: 0,
+            totais: 28,
         }
-    }
+    },
+    'LIN600': {
+        //Ingles 6(GTI)
+        titulo: 'Inglês VI',
+        apresentacao: 'A Matéria irá abordar conteudo sobre Inglês',
+        area: 'Linguas',
+        ementa: 'Adjective, adverb.\nShort Adjectives. Comparative of equality and inferiority.\nShort Adjs. Comparative of superiority. Superlative of inferiority and superiority.\nLong Adjectives. Comparative of equality and inferiority.\nLong Adjs. Comparative of superiority. Superlative of inferiority and superiority.',
+        objetivo: 'Fazer o Aluno ver outra lingua que é a mais falado no mundo, para que ele possa ler/escrever/falar nesse idioma',
+        cargas: {
+            semanais: 2,
+            teoricas: 14,
+            praticas: 0,
+            totais: 14,
+        }
+    },
+    'HST002': {
+        //SOCTEC
+        titulo: 'Sociedade e Tecnologia',
+        apresentacao: 'A Matéria irá abordar sobre como a sociedade e a teconologia andam lado a lado desdos inicio da humanidade',
+        area: 'Humanas',
+        ementa: 'Livro A Metamorfose\nIntrodução ao pensamento científico sobre o social.\nRelações de trabalho e sociedade industrial\nRelatório do filme/livro A Metamorfose\nDesenvolvimento tecnológico, política e sociedade.\nTrabalho e militarização da ciência e tecnologia',
+        objetivo: 'Fazer com que o Aluno de TI entenda sobre a Sociedade como um todo',
+        cargas: {
+            semanais: 2,
+            teoricas: 14,
+            praticas: 0,
+            totais: 14,
+        }
+    },
+    'AGR101': {
+        //Gestão de Equipes
+        titulo: 'Gestão de Equipes',
+        apresentacao: 'A matéria irá abordar sobre como fazer uma equipe e gerir ela.',
+        area: 'Humanas',
+        ementa: 'TRANSFORMAÇÃO humana x empresarial\nCompetitividade Organizacional baseado na participação do individuo\nMercado em Oferta e Mercado em Demanda.\nTríade de atuação em GP\nAndragógia\nCompetencias\nAprendizagem organizacional',
+        objetivo: 'Compreender e aplicar conceitos sobre como gerir e fazer parte de uma equipe',
+        cargas: {
+            semanais: 2,
+            teoricas: 14,
+            praticas: 0,
+            totais: 14,
+        }
+    },
+    'MMD001': {
+        //MatDisc
+        titulo: 'Matemática Discreta',
+        apresentacao: 'Matemática Discreta:\nProvê um conjunto de técnicas para modelar problemas em Ciência da Computação.',
+        area: 'Exatas',
+        ementa: 'Teoria dos conjuntos. Indução matemática. Análise combinatória. Lógica formal. Relações. Funções. Grafos e árvores.',
+        objetivo: 'Compreender e aplicar os conceitos fundamentais da matemática para computação em situações-problema dentro do contexto do curso.',
+        cargas: {
+            semanais: 4,
+            teoricas: 14,
+            praticas: 14,
+            totais: 28,
+        }
+    },
+    'IED001': {
+        //Estrutura de Dados
+        titulo: 'Estrutura de Dados',
+        apresentacao: 'Criar e manipular tipos abstratos de dados:\nlistas, pilhas, filas e árvores.\nSelecionar estruturas de dados adequadas para solução conveniente de problemas.',
+        area: 'Lógico',
+        ementa: 'Alocação sequencial e não sequencial, pilhas, filas, listas, encadeamento, recursividade, árvores e tabelas de espalhamento.',
+        objetivo: 'Revisão de Java:\ntipos, variáveis, estruturas de controle, métodos e arrays.\n2. Conceitos Básicos de Estruturas de Dados:\ndefinições; tipos de dados; tipos abstratos de dados.\n3. Listas Lineares:\ndefinição, operações, algoritmos, construção e aplicação.\n4. Pilhas:\ndefinição, operações, construção e aplicação.\n5. Filas:\ndefinição, operações, construção e aplicação.\n6. Ordenação & Busca:\ndefinição, avaliação de custo, algoritmos, aplicação.\n7. Árvores:\ndefinição, terminologia, construção, percursos e aplicação.\n8. Tabelas de espalhamento:\ndefinição, construção e aplicação.',
+        cargas: {
+            semanais: 4,
+            teoricas: 14,
+            praticas: 14,
+            totais: 28,
+        }
+    },
+    'ILP010': {
+        //Linguagem de Programação
+        titulo: 'Linguagem de Programação',
+        apresentacao: 'Lógica\nLógica de programação\nAlgoritmos\nFluxograma (diagrama de blocos)\nPortuguês estruturado (pseudocódigo)\nLinguagens de Programação\nParadigmas\nProg. Estruturada\nProg. Orientada a Objeto',
+        area: 'Lógico',
+        ementa: 'Revisão dos conceitos básicos\nVetores\nMatrizes\nManipulação de Strings\nFunções (métodos)\nAcesso à Arquivos\nGUI',
+        objetivo: 'Solucionar problemas utilizando a lógica de programação e a implementação de programas por meio de uma linguagem de programação.\nSe especializar em Java\nIniciar o desenvolvimento de software com GUI',
+        cargas: {
+            semanais: 4,
+            teoricas: 14,
+            praticas: 14,
+            totais: 28,
+        }
+    },
 };
--- dados/materiais.js
+++ dados/materiais.js
@@ -9,6 +9,17 @@
 
 */
 
+/*  EXEMPLO
+'': [
+        {
+            tipoArquivo: '',
+            nome: '',
+            descricao: '',
+            link: 'http://google.com',
+        },
+    ],
+*/
+
 module.exports = {
     'HSO003': [
         {
@@ -39,5 +50,101 @@ module.exports = {
             descricao: 'GGTI-01 Arquivo de apoio',
             link: 'http://google.com',
         },
-    ]
+    ],
+    'ILP010': [
+        {
+            tipoArquivo: 'PDF',
+            nome: 'Apresentação da disciplina',
+            descricao: 'Apresentação da disciplina',
+            link: 'http://google.com',
+        },
+        {
+            tipoArquivo: 'PDF',
+            nome: 'Aula 1 - Introdução ao Java',
+            descricao: 'Aula 1 - Introdução ao Java',
+            link: 'http://google.com',
+        },
+        {
+            tipoArquivo: 'PDF',
+            nome: 'Lista 1 - Revisão',
+            descricao: 'Lista 1 - Revisão em Java',
+            link: 'http://google.com',
+        },
+    ],
+    'IED001': [
+        {
+            tipoArquivo: 'PDF',
+            nome: 'ED-2017-00S2-Disciplina',
+            descricao: 'Apresentação da disciplina',
+            link: 'http://google.com',
+        },
+        {
+            tipoArquivo: 'PDF',
+            nome: 'ED-2017-01-Intro-Java',
+            descricao: 'Introdução ao Java',
+            link: 'http://google.com',
+        },
+        {
+            tipoArquivo: 'PDF',
+            nome: 'ED-2017-02-Arrays',
+            descricao: 'Arrays uni e multidimensionais.',
+            link: 'http://google.com',
+        },
+    ],
+    'AGR101': [
+        {
+            tipoArquivo: 'PDF',
+            nome: 'TRABALHO DE GESTÃO DE EQUIPES',
+            descricao: 'ENTREGA para P1 - 1 capitulo \nEntrega para P2 - Trabalho finalizado \ntrabalhoseprojetosfatec@outlook.com',
+            link: 'http://google.com',
+        },
+    ],
+    'MMD001': [
+        {
+            tipoArquivo: 'PDF',
+            nome: 'Apostila de MATEMÁTICA DISCRETA',
+            descricao: 'Material a ser utilizado nas aulas de MATEMÁTICA DISCRETA. Providenciar a versão impressa ou eletrônica.',
+            link: 'http://google.com',
+        },
+        {
+            tipoArquivo: 'PDF',
+            nome: 'Teoria dos Conjuntos',
+            descricao: 'Material Complementar.',
+            link: 'http://google.com',
+        },
+        {
+            tipoArquivo: 'PDF',
+            nome: 'Informações Importantes',
+            descricao: '- Ementas\n- Bibliografias\n- Datas de provas\n- Critérios de avaliação',
+            link: 'http://google.com',
+        },
+    ],
+    'LIN006': [
+        {
+            tipoArquivo: 'PDF',
+            nome: 'Aula1',
+            descricao: '',
+            link: 'http://google.com',
+        },
+        {
+            tipoArquivo: 'PDF',
+            nome: 'Aula2',
+            descricao: '',
+            link: 'http://google.com',
+        },
+    ],
+    'HST002': [
+        {
+            tipoArquivo: 'PDF',
+            nome: 'Aula 1',
+            descricao: '',
+            link: 'http://google.com',
+        },
+        {
+            tipoArquivo: 'PDF',
+            nome: 'Aula 2',
+            descricao: '',
+            link: 'http://google.com',
+        },
+    ],
 };
